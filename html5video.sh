e521b8d38030e98665d23e25e0eec54ab2765ede
Piotr Kierzniewski <kierzniak@gmail.com>
2015-03-10 11:39:23 +0100

    Typo

--- html5video.sh
+++ html5video.sh
@@ -19,7 +19,7 @@ ffmpeg -i $1.mp4 -pass 1 -codec:v libvpx -b:v 512k -codec:a libvorbis -b:a 128k 
 ffmpeg -i $1.mp4 -pass 2 -codec:v libvpx -b:v 512k -codec:a libvorbis -b:a 128k -s 640x360 -y $1_640x360.webm
 
 #mp4
-ffmpeg -i $1.mp4 -pass 1 -codec:v libx264 -b:v 3072k -codec:a libfaac -b:a 192k -movflags +faststart -pix_fmt yuv420p -s 1920x1080 -f webm -y /dev/null
+ffmpeg -i $1.mp4 -pass 1 -codec:v libx264 -b:v 3072k -codec:a libfaac -b:a 192k -movflags +faststart -pix_fmt yuv420p -s 1920x1080 -f mp4 -y /dev/null
 ffmpeg -i $1.mp4 -pass 2 -codec:v libx264 -b:v 3072k -codec:a libfaac -b:a 192k -movflags +faststart -pix_fmt yuv420p -s 1920x1080 -y $1_1920x1080.mp4
 
 ffmpeg -i $1.mp4 -pass 1 -codec:v libx264 -b:v 2048k -codec:a libfaac -b:a 192k -movflags +faststart -pix_fmt yuv420p -s 1280x720 -f mp4 -y /dev/null
